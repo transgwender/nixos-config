9a53e482ec5c7ff20148f897960e3fb2a6cddf23
Gwendolyn Kornak <gwen@gwenkornak.ca>
2025-05-20 20:43:51 -0700

    add dns

--- modules/network.nix
+++ modules/network.nix
@@ -13,7 +13,7 @@
   networking.nat.externalInterface = "wlp4s0";
   networking.nat.internalInterfaces = [ "wg0" ];
   networking.firewall = {
-    allowedUDPPorts = [ 51820 ];
+    allowedUDPPorts = [ 53 51820 ];
   };
 
   networking.wireguard.interfaces = {
@@ -49,6 +49,38 @@
           allowedIPs = [ "10.100.0.3/32" ];
         }
       ];
+    };
+  };
+
+  # DNS proxy and ad-blocker
+  services.blocky = {
+    enable = true;
+    settings = {
+      ports.dns = 53; # Port for incoming DNS Queries.
+      upstreams.groups.default = [
+        "https://one.one.one.one/dns-query" # Using Cloudflare's DNS over HTTPS server for resolving queries.
+      ];
+      # For initially solving DoH/DoT Requests when no system Resolver is available.
+      bootstrapDns = {
+        upstream = "https://one.one.one.one/dns-query";
+        ips = [ "1.1.1.1" "1.0.0.1" ];
+      };
+      #Enable Blocking of certian domains.
+      blocking = {
+        blackLists = {
+          #Adblocking
+          ads = ["https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"];
+        };
+        #Configure what block categories are used
+        clientGroupsBlock = {
+          default = [ "ads" ];
+        };
+      };
+      caching = {
+        minTime = "5m";
+        maxTime = "30m";
+        prefetching = true;
+      };
     };
   };
 }
